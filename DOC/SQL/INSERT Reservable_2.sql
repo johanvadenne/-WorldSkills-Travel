1fae83531b1cd525b63a82c2e477540dea63ca85
johanvadenne <106351932+johanvadenne@users.noreply.github.com>
2024-10-19 09:00:41 +0200

    1.2

---
+++ DOC/SQL/INSERT Reservable_2.sql
@@ -0,0 +1,16 @@
+
+INSERT INTO public."T_Reservable"("NomReservable", "Prix", "Pays", "Region", "Ville", "Rue ...", "IdCompteUtilisateurProprietaire", "LienImage") VALUES 
+('Campigne en forêt', '200.00', 'France', 'Bourgogne-Franche-Comté', 'Bangkok', 'Rue ...', 1, 'pexels-ahmetyuksek-28971101.jpg'),
+('Natation', '10.00', 'France', 'Bretagne', 'Paris', 'Rue ...', 1, 'pexels-ajaybhargavguduru-863988.jpg'),
+('Maison familiale', '300.00', 'France', 'Centre-Val de Loire', 'Londres', 'Rue ...', 1, 'pexels-binyaminmellish-186077.jpg'),
+('Maison familiale', '250.00', 'France', 'Corse', 'Dubaï', 'Rue ...', 1, 'pexels-binyaminmellish-1396122.jpg'),
+('yoga', '20.00', 'France', 'Grand Est', 'Singapour', 'Rue ...', 1, 'pexels-chevanon-317157.jpg'),
+('Maison de campagne', '300.00', 'France', 'Hauts-de-France', 'New York', 'Rue ...', 1, 'pexels-curtis-adams-1694007-3935333.jpg'),
+('Culture espagnole', '15.00', 'France', 'Île-de-France', 'Kuala Lumpur', 'Rue ...', 1, 'pexels-eduardo-hernandez-soto-1315528681-28954840.jpg'),
+('Voyage Guadeloupe', '500.00', 'France', 'Guadeloupe', 'Tokyo', 'Rue ...', 1, 'pexels-jakub-krystkiewicz-1693170418-28983097.jpg'),
+('Quartier Bordeaux', '122.00', 'France', 'Normandie', 'Istanbul', 'Rue ...', 1, 'pexels-michael-tuszynski-1134777-2157404.jpg'),
+('Appartement 3 pièce', '110.00', 'France', 'Nouvelle-Aquitaine', 'Séoul', 'Rue ...', 1, 'pexels-mikebirdy-144632.jpg'),
+('Marché nocturne Angers', '00.00', 'France', 'Occitanie', 'Hong Kong', 'Rue ...', 1, 'pexels-necatiomerk-28977063.jpg'),
+('Marathon', '20.00', 'France', 'Pays de la Loire', 'Barcelone', 'Rue ...', 1, 'pexels-pixabay-221210.jpg'),
+('Tour du monde', '00.00', 'France', 'Provence-Alpes-Côte d''Azur', 'Amsterdam', 'Rue ...', 1, 'pexels-pixabay-248547.jpg');
+
